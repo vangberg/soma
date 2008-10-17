9976c1497a102532a3eb1da7b58dd477d0707c6a
ichverstehe <ichverstehe@gmail.com>
2008-10-17 14:38:00 +0200

    line history etc..

--- soma.rb
+++ soma.rb
@@ -8,15 +8,15 @@ class Soma
   end
 
   def erase_and_open_buffer
-    @file = File.open(File.join(Dir::tmpdir, "#{`whoami`.strip}_soma"), 'w+')
+    @file = File.open(File.join(Dir::tmpdir, "#{`whoami`.strip}_somarepl_buffer"), 'w+')
   end
 
   def tail
     erase_and_open_buffer
     loop do
-      while (line = @file.read) && !line.empty?
+      while (line = @file.readlines) && !line.empty?
+        line.each {|l| Readline::HISTORY.push(l.strip) }
         IRB::Irb.new(nil, @file.path).eval_input
-        Readline::HISTORY.push(line.strip)
         erase_and_open_buffer
       end
       sleep 0.2
