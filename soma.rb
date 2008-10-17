619fd0c77158954a4f28f828d421cfdd57b2f059
ichverstehe <ichverstehe@gmail.com>
2008-10-17 14:03:59 +0200

    refactor

--- soma.rb
+++ soma.rb
@@ -1,22 +1,25 @@
 require 'tmpdir'
 
+# Add Soma.new.start to your ~/.irbrc
+
 class Soma
-  def initialize
+  def start
     Thread.start { tail }
   end
 
-  def tail
-    begin
+  def erase_and_open_buffer
     @file = File.open(File.join(Dir::tmpdir, "#{`whoami`.strip}_soma"), 'w+')
-      loop do
-        while (line = @file.read) && !line.empty?
-          Readline::HISTORY.push(line.strip)
-          IRB::Irb.new(nil, @file.path).eval_input
-          @file = File.open(File.join(Dir::tmpdir, "#{`whoami`.strip}_soma"), 'w+')
+  end
 
-        end
-        sleep 1
+  def tail
+    erase_and_open_buffer
+    loop do
+      while (line = @file.read) && !line.empty?
+        IRB::Irb.new(nil, @file.path).eval_input
+        Readline::HISTORY.push(line.strip)
+        erase_and_open_buffer
       end
+      sleep 0.2
     end
   end
 end
