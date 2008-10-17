c735264c027794e2c0440fc5cdc8a152f78658bf
ichverstehe <ichverstehe@gmail.com>
2008-10-17 21:15:21 +0200

    module, thread, bla bla

--- lib/soma.rb
+++ lib/soma.rb
@@ -4,9 +4,10 @@ class IRB::Irb
   attr_writer :context
 end
 
-class Soma
+module Soma
+  extend self
   def start
-    Thread.start { tail }
+    @thread || @thread = Thread.start { tail }
   end
 
   def erase_and_open_buffer
@@ -18,10 +19,14 @@ class Soma
     loop do
       while (line = @file.readlines) && !line.empty?
         line.each {|l| Readline::HISTORY.push(l.strip) }
+        # Get the current IRB
         irb = IRB.CurrentContext.irb
+        # We save the IRB context that takes STDIN as input
         stdin_context = irb.context
+        # .. replace it with a context that takes our buffer file as input
         irb.context = IRB::Context.new(irb, stdin_context.workspace, @file.path)
         irb.eval_input
+        # and finally puts back the STDIN context so control is handled back to the IRB session again
         irb.context = stdin_context
         erase_and_open_buffer
       end
